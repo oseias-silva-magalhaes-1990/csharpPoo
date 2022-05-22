14722e813b7843be50691d95b80e36b2a477d732
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 15:38:55 -0300

    Update ByteBank.cs

--- ByteBank/07 - Construtores/ByteBank.cs
+++ ByteBank/07 - Construtores/ByteBank.cs
@@ -1,1 +1,23 @@
+using System;
 
+namespace _06_ByteBank
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+        ContaCorrente conta = new ContaCorrente();
+        Cliente cliente = new Cliente();
+          
+        cliente.Nome = "Guilherme";
+        cliente.CPF = "123.456.789-10";
+        cliente.Profissao = "Desenvolvedor";
+        
+        conta.Saldo = -10;
+        
+        Console.WriteLine(conta.Saldo);
+        Console.WriteLine(conta.Titular.Nome);
+        Console.ReadLine();
+    }
+  }
+}
