ea851905f64c2d07d4cf555175f93058936f7afd
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 14:54:33 -0300

    Update ByteBank.cs

--- ByteBank/06 - ObterSaldo e DefinirSaldo/ByteBank.cs
+++ ByteBank/06 - ObterSaldo e DefinirSaldo/ByteBank.cs
@@ -9,7 +9,7 @@ namespace _06_ByteBank
         ContaCorrente conta1 = new ContaCorrente();
         conta1.DefinirSaldo(-10);
         
-        Console.WriteLine(conta1.saldo);
+        Console.WriteLine(conta1.ObterSaldo());
         Console.ReadLine();
     }
   }
