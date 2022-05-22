8b4340b79b7014347387a72fbd0792188d6d3c50
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 14:23:28 -0300

    Update ByteBank.cs

--- ByteBank/05 - Relacionamento entre Classes/ByteBank.cs
+++ ByteBank/05 - Relacionamento entre Classes/ByteBank.cs
@@ -10,6 +10,17 @@ namespace _05_ByteBank
         cliente1.nome = "Gabriela";
         cliente1.profissao = "Desenvolvedora C#";
         cliente1.cpf = "123.456.789-10";
+      
+        ContaCorrente conta1 = new ContaCorrente();
+        conta1.titular = cliente1;//Atribui objeto da classe Cliente à objeto da classe ContaCorrente
+        conta1.saldo = 563.0;
+        conta1.agencia = 563;
+        conta1.numero = 5634527;
+        
+        Console.WriteLine(cliente1.nome);//Imprime Valor Atribuido
+        Console.WriteLine(conta1.titular.nome);//Imprime valor passado por referência
+      
+        Console.ReadLine();
     }
   }
 }
