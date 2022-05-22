d634d6e5e3188665ee7c7d3589abb57c4e3eaadd
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 11:09:50 -0300

    Update 01-byte-bank.cs

--- 01-byte-bank.cs
+++ 01-byte-bank.cs
@@ -1,13 +1,14 @@
+using System;
+
 namespace _01_ByteBank
 {
   class Program
   {
     static void Main (string[] args)
     {
-      string titular;
-      int numeroAgencia;
-      int numero;
-      double saldo;
+      new ContaCorrente();
+      
+      Console.ReadLine();
     }
   }
 }
