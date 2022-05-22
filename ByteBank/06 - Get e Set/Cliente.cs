e60eb0c1e638b746542aaafbecef476951c41e4f
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 15:37:00 -0300

    Update Cliente.cs

--- ByteBank/06 - Get e Set/Cliente.cs
+++ ByteBank/06 - Get e Set/Cliente.cs
@@ -4,8 +4,21 @@ namespace _06_ByteBank
 {
   public class Cliente
   {
+    private string _cpf;
+    
     public string Nome{get; set;}
-    public string CPF{get; set;}
+    public string CPF{
+      get
+      {
+        return this._cpf;
+      }
+      set
+      {
+        //Escrever validação de CPF
+        this._cpf = value;
+      }
+    }
+    
     public string Profissão{get; set;}
   }
 }
