39368aea5e6d3322edd5f61ff5072a8c7b9603d7
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 15:35:21 -0300

    Update ByteBank.cs

--- ByteBank/06 - Get e Set/ByteBank.cs
+++ ByteBank/06 - Get e Set/ByteBank.cs
@@ -7,9 +7,16 @@ namespace _06_ByteBank
     static void Main (string[] args)
     {
         ContaCorrente conta = new ContaCorrente();
+        Cliente cliente = new Cliente();
+          
+        cliente.Nome = "Guilherme";
+        cliente.CPF = "123.456.789-10";
+        cliente.Profissao = "Desenvolvedor";
+        
         conta.Saldo = -10;
         
         Console.WriteLine(conta.Saldo);
+        Console.WriteLine(conta.Titular.Nome);
         Console.ReadLine();
     }
   }
