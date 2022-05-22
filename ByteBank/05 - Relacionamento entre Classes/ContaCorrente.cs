517da6617690aa1b89a46f01c10f3f33bac4c8e5
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 14:09:29 -0300

    Update ContaCorrente.cs

--- ByteBank/05 - Relacionamento entre Classes/ContaCorrente.cs
+++ ByteBank/05 - Relacionamento entre Classes/ContaCorrente.cs
@@ -1,33 +1,36 @@
 using System;
-using _05_ByteBank;
 
-public class ContaCorrente
+namespace _05_ByteBank
 {
-  public Cliente titular;
-  public int agencia;
-  public int numero;
-  public double saldo = 100;
-  
-  public bool Sacar(double valor){
-    if(this.saldo < valor){
-      return false;
-    }
-    this.saldo -= valor;
-    return true;
-  }
-  
-  public void Depositar(double valor){
-    this.saldo += valor;
-	Console.WriteLine("Depositou");
-  }
-  
-  public bool Transferir(double valor, ContaCorrente contaDestino){
-    if(this.saldo < valor){
-      return false;
-    }
-    this.saldo -= valor;
-    contaDestino.Depositar(valor);
-    return true;
-  }
-  
+	public class ContaCorrente
+	{
+		  public Cliente titular;
+		  public int agencia;
+		  public int numero;
+		  public double saldo = 100;
+
+		  public bool Sacar(double valor){
+			if(this.saldo < valor){
+				return false;
+			}
+			  
+			this.saldo -= valor;
+			return true;
+		  }
+
+		  public void Depositar(double valor){
+		  	this.saldo += valor;
+			Console.WriteLine("Depositou");
+		  }
+
+		  public bool Transferir(double valor, ContaCorrente contaDestino){
+		  	if(this.saldo < valor){
+		  		return false;
+		  	}
+			  
+		  	this.saldo -= valor;
+		  	contaDestino.Depositar(valor);
+		  	return true;
+		  }
+	}
 }
