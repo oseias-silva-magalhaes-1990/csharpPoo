22bcff5c9c601b39d285160b6484510407196bf1
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 11:41:42 -0300

    Create ByteBank.cs

---
+++ ByteBank/02/ByteBank.cs
@@ -0,0 +1,21 @@
+namespace _01_ByteBank
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+      ContaCorrente conta = new ContaCorrente();
+      conta.titular = "Gabriela";
+      conta.agencia = 863;
+      conta.numero = 863452;
+      conta.saldo = 100.0;
+      
+      Console.WriteLine("Nome: " + conta.titular);
+	  Console.WriteLine("Agência: " + conta.agencia);
+      Console.WriteLine("Conta: " + conta.numero);
+      Console.WriteLine("Saldo: " + conta.saldo);
+      
+      Console.ReadLine();
+    }
+  }
+}
