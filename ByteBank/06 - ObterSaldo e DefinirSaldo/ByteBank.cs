795ed891f3be360ea2db111ef1310bce50037871
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 14:36:03 -0300

    Create ByteBank.cs

---
+++ ByteBank/06 - ObterSaldo e DefinirSaldo/ByteBank.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace _06_ByteBank
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+        //Cliente cliente1 = new Cliente();
+      
+        //cliente1.nome = "Gabriela";
+        //cliente1.profissao = "Desenvolvedora C#";
+        //cliente1.cpf = "123.456.789-10";
+      
+        ContaCorrente conta1 = new ContaCorrente();
+      
+        //conta1.titular = cliente1;//Atribui objeto da classe Cliente à objeto da classe ContaCorrente
+        
+        conta1.titular = new Cliente();
+        conta1.titular.nome = "Gabriela";
+        conta1.titular.profissao = "Desenvolvedora C#";
+        conta1.titular.cpf = "123.456.789-10";
+      
+        conta1.saldo = 563.0;
+        conta1.agencia = 563;
+        conta1.numero = 5634527;
+        
+        //Console.WriteLine(cliente1.nome);//Imprime Valor Atribuido
+        Console.WriteLine(conta1.titular.nome);//Imprime valor passado por referência
+        Console.WriteLine(conta1.titular.cpf);
+        Console.WriteLine(conta1.titular.profissao);
+      
+        Console.ReadLine();
+    }
+  }
+}
