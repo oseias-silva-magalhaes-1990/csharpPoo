84979de02d0a1cbb8ab5ab3b5061c2807bbb1f35
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 13:19:44 -0300

    Update ByteBank.cs

--- ByteBank/04 -Métodos e Funções/ByteBank.cs
+++ ByteBank/04 -Métodos e Funções/ByteBank.cs
@@ -12,17 +12,18 @@ namespace _04_ByteBank
 			conta1.numero = 863452;
 			
 	    		Console.WriteLine("Depositou? " + conta1.Depositar(100.0));
-            		Console.WriteLine("Saldo: " + conta1.saldo);
+            		Console.WriteLine("Conta 1 - Saldo: " + conta1.saldo);
             
             		Console.WriteLine("Sacou? " + conta1.Sacar(50.0));
-            		Console.WriteLine("Saldo: " + conta1.saldo);
+            		Console.WriteLine("Conta 1 - Saldo: " + conta1.saldo);
 	    		
 	    		ContaCorrente conta2 = new ContaCorrente();
-	    		conta1.titular = "Bruno";
-			conta1.agencia = 864;
-			conta1.numero = 864324;
+	    		conta2.titular = "Bruno";
+			conta2.agencia = 864;
+			conta2.numero = 864324;
 	    		
 	    		conta1.Transferir(50.0, conta2);
+	    		Console.WriteLine("Conta 2 - Saldo" + conta2.saldo);
 
 			Console.ReadLine();
 		}
