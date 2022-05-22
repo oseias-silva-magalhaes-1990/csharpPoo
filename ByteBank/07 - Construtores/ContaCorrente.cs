af7d867be85ce13f9b0423f53b79d93778f4c136
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 15:40:04 -0300

    Create ContaCorrente.cs

---
+++ ByteBank/07 - Construtores/ContaCorrente.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace _06_ByteBank
+{
+	public class ContaCorrente
+	{
+		public Cliente Titular {get; set;}
+		public int Agencia{get; set;}
+		public int Numero{get; set;}
+		
+		private double _saldo = 100;
+		public double Saldo
+		{
+			get
+			{
+				return this._saldo;
+			}
+			set
+			{
+				if(value < 0)
+			{
+				Console.WriteLine("Erro! Saldo negativo atribuído!");
+				return;	
+			}
+				this._saldo = value; 
+			}
+		}
+
+		  public bool Sacar(double valor){
+			  
+			if(this._saldo < valor){
+				return false;
+			}
+			  
+			this._saldo -= valor;
+			return true;
+		  }
+
+		  public void Depositar(double valor){
+		  	this._saldo += valor;
+			Console.WriteLine("Depositou");
+		  }
+
+		  public bool Transferir(double valor, ContaCorrente contaDestino){
+		  	if(this._saldo < valor){
+		  		return false;
+		  	}
+			  
+		  	this._saldo -= valor;
+		  	contaDestino.Depositar(valor);
+		  	return true;
+		  }
+	}
+}
