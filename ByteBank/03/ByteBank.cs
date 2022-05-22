bcf77c3daae8f9cb947d88ec582c5b3886a56cf4
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 12:05:14 -0300

    Update ByteBank.cs

--- ByteBank/03/ByteBank.cs
+++ ByteBank/03/ByteBank.cs
@@ -2,22 +2,33 @@ using System;
 
 namespace _03_ByteBank
 {
-  class Program
-  {
-    static void Main (string[] args)
-    {
-      ContaCorrente conta = new ContaCorrente();
-      conta.titular = "Gabriela";
-      conta.agencia = 863;
-      conta.numero = 863452;
-      conta.saldo = 100.50;
-      
-      Console.WriteLine("Nome: " + conta.titular);
-	  Console.WriteLine("Agência: " + conta.agencia);
-      Console.WriteLine("Conta: " + conta.numero);
-      Console.WriteLine("Saldo: " + conta.saldo);
-      
-      Console.ReadLine();
-    }
-  }
+	class Program
+	{
+		static void Main (string[] args)
+		{
+			ContaCorrente conta1 = new ContaCorrente();
+			conta1.titular = "Gabriela";
+			conta1.agencia = 863;
+			conta1.numero = 863452;
+			conta1.saldo = 100.50;
+
+			ContaCorrente conta2 = new ContaCorrente();
+			conta2.titular = "Gabriela";
+			conta2.agencia = 863;
+			conta2.numero = 863452;
+			conta2.saldo = 100.50;
+
+			Console.WriteLine("Igualdade de tipo de referência: " + (conta1 == conta2));
+
+			int idade1 = 27;
+			int idade2 = 27;
+			
+			Console.WriteLine("Igualdade de tipo de Valor: " + (idade1 == idade2));
+			
+			conta1 = conta2;//Atribuição de objetos - referência de posição na memória computacional alterada
+			Console.WriteLine("Igualdade de tipo de referência: " + (conta1 == conta2));
+			
+			Console.ReadLine();
+		}
+	}
 }
