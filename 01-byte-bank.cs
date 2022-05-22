890986c4c1fd9cb6d13b91985ab4bcbedd467ae0
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-22 11:00:47 -0300

    Create 01-byte-bank.cs

---
+++ 01-byte-bank.cs
@@ -0,0 +1,13 @@
+namespace _01_ByteBank
+{
+  class Program
+  {
+    static void Main (string[] args)
+    {
+      string titular;
+      int numeroAgencia;
+      int numero;
+      double saldo;
+    }
+  }
+}
