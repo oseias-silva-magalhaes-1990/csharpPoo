5715c29f70beba4b153944b2538df3e52d855623
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-23 16:23:39 -0300

    Update Funcionario.cs

--- ByteBank/08 - Herança e Interface/Funcionário/Funcionario.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/Funcionario.cs
@@ -4,19 +4,9 @@ namespace ByteBank.Funcionarios
 {
   public class Funcionario
   {
-    //0 - Operacional
-    //1 - Diretor
-    //2 - Designer
-    //N - ...
-    private int _tipo;
-    
     public string Nome {get; set;}
     public string CPF {get; set;}
     public double Salario {get; set;}
-    
-    public Funcionario(int tipo){
-      _tipo = tipo;
-    }
     
     public double GetBonificacao()
     {
