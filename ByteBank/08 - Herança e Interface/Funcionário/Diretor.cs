dc8ab9880d977a2d2b8efc0a71ab3828da07ce8f
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-23 16:25:05 -0300

    Create Diretor.cs

---
+++ ByteBank/08 - Herança e Interface/Funcionário/Diretor.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace ByteBank.Funcionarios
+{
+  public class Diretor
+  {
+    public string Nome {get; set;}
+    public string CPF {get; set;}
+    public double Salario {get; set;}
+    
+    public double GetBonificacao()
+    {
+      return Salario;
+    }
+  }
+}
