3c9c7457e22a5dc70e83c382847cfa0095f74054
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-23 17:21:59 -0300

    Update Funcionario.cs

--- ByteBank/08 - Herança e Interface/Funcionário/Funcionario.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/Funcionario.cs
@@ -4,9 +4,17 @@ namespace ByteBank
 {
   public class Funcionario
   {
+    public static int TotalDeFuncionarios{get; private set;}
+    
     public string Nome {get; set;}
-    public string CPF {get; set;}
+    public string CPF {get; private set;}
     public double Salario {get; set;}
+    
+    public Funcionario(string cpf)
+    {
+      CPF = cpf;
+      TotalDeFuncionarios++;
+    }
     
     public virtual double GetBonificacao()//Definindo como virtual o método pode ser utilziado por uma classe derivada e sofrer mudanças em sua estrutura
     {
