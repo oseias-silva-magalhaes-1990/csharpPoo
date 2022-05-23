4f4e51f14d865b1ca21c569987db33f1157aca84
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-23 16:32:38 -0300

    Update ByteBank.cs

--- ByteBank/08 - Herança e Interface/Funcionário/ByteBank.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/ByteBank.cs
@@ -7,21 +7,27 @@ namespace ByteBank
   {
     static void Main(string[] args)
     {
+      GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
+      
       Funcionario func = new Funcionario();//Atalho para coreção de Biblioteca (ctrl + . + enter)
       func.Nome = "Carlos";
       func.CPF = "379.721.748-25";
       func.Salario = 2000;
       
-      Diretor diret = new Diretor();//Atalho para coreção de Biblioteca (ctrl + . + enter)
-      diret.Nome = "Roberta";
-      diret.CPF = "374.731.747-36";
-      diret.Salario = 5000;
+      gerenciador.Registrar(func);
+      
+      Diretor dire = new Diretor();//Atalho para coreção de Biblioteca (ctrl + . + enter)
+      dire.Nome = "Roberta";
+      dire.CPF = "374.731.747-36";
+      dire.Salario = 5000;
+      
+      gerenciador.Registrar(dire);
       
       Console.WriteLine(func.Nome);
       Console.WriteLine(func.GetBonificacao());
       
-      Console.WriteLine(diret.Nome);
-      Console.WriteLine(diret.GetBonificacao());
+      Console.WriteLine(dire.Nome);
+      Console.WriteLine(dire.GetBonificacao());
       
       Console.ReadLine();
       
