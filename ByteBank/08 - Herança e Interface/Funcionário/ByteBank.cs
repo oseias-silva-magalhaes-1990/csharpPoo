16df10dcd91f35edc5378c794a7d26c03097d522
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-23 16:26:28 -0300

    Update ByteBank.cs

--- ByteBank/08 - Herança e Interface/Funcionário/ByteBank.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/ByteBank.cs
@@ -12,8 +12,16 @@ namespace ByteBank
       func.CPF = "379.721.748-25";
       func.Salario = 2000;
       
+      Diretor diret = new Diretor();//Atalho para coreção de Biblioteca (ctrl + . + enter)
+      diret.Nome = "Roberta";
+      diret.CPF = "374.731.747-36";
+      diret.Salario = 5000;
+      
       Console.WriteLine(func.Nome);
       Console.WriteLine(func.GetBonificacao());
+      
+      Console.WriteLine(diret.Nome);
+      Console.WriteLine(diret.GetBonificacao());
       
       Console.ReadLine();
       
