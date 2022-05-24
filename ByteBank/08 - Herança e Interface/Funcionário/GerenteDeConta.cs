dbc992b3bbcaf521e7c886b5310a367f86be1a96
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-24 17:19:12 -0300

    Update GerenteDeConta.cs

--- ByteBank/08 - Herança e Interface/Funcionário/GerenteDeConta.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/GerenteDeConta.cs
@@ -5,7 +5,6 @@ namespace ByteBank.Funcionario
 {
   public class GerenteDeConta : FuncionarioAutenticavel//Herança - herda atributos da classe base
   {
-  
     public GerenteDeConta(string cpf) : base (4000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
     {
       Console.WriteLine("Criando um GerenteDeConta");
