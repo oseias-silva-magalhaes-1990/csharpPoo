bdb173c453cd5a8b2270d43cea8a3a4d90a5d86c
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-24 13:49:14 -0300

    Create GerenteDeConta.cs

---
+++ ByteBank/08 - Herança e Interface/Funcionário/GerenteDeConta.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace ByteBank
+{
+  public class GerenteDeConta : Funcionario //Herança - herda atributos da classe base
+  {
+    
+    public GerenteDeConta(double salario, string cpf) : base (4000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
+    {
+      Console.WriteLine("Criando um GerenteDeConta");
+    }
+  
+    public override double GetBonificacao()//Definindo como override informa-se que o método é parte de uma classe pai
+    {
+      return Salario * 0.25;
+    }
+    
+    public override void AumentarSalario()
+    {
+      Salario *= 1.05;
+    }
+    
+  }
+}
