1b6eae00c954b192ab95b19c8773d8bb1e333f3c
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-24 15:30:08 -0300

    Update Diretor.cs

--- ByteBank/08 - Herança e Interface/Funcionário/Diretor.cs
+++ ByteBank/08 - Herança e Interface/Funcionário/Diretor.cs
@@ -5,17 +5,10 @@ namespace ByteBank.Funcionario
   public class Diretor : Funcionario //Herança - herda atributos da classe base
   {
     
-    public string Senha{get; set;}
-    
     public Diretor(string cpf) : base (5000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
     {
       Console.WriteLine("Criando um Diretor");
     }
-    
-    public bool Autenticar(string senha){
-      return Senha == senha;
-    }
-    
   
     public override double GetBonificacao()//Definindo como override informa-se que o método é parte de uma classe pai
     {
