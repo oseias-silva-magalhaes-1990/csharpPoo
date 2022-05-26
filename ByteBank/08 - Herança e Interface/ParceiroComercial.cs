8c5cbc02cd629954e1aeecb7f2261971de5826ed
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-26 13:55:16 -0300

    Update ParceiroComercial.cs

--- ByteBank/08 - Herança e Interface/ParceiroComercial.cs
+++ ByteBank/08 - Herança e Interface/ParceiroComercial.cs
@@ -2,7 +2,7 @@ using System;
 
 namespace ByteBank
 {
-  public class ParceiroComercial : Autenticavel
+  public class ParceiroComercial : IAutenticavel
   {
     public string Senha {get; set;}
     public bool Autenticar(string senha)
