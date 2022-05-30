752ac17493ebe02d7d406ed3c8bc7812d5b1243c
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-05-30 15:57:31 -0300

    Update ContaCorrente.cs

--- ByteBank/08 - Herança e Interface/ContaCorrente.cs
+++ ByteBank/08 - Herança e Interface/ContaCorrente.cs
@@ -18,13 +18,13 @@ namespace _07_ByteBank
 			if(agencia <= 0)
 			    {
 				//Lançamento de exceção de argumento
-				throw new ArgumentException("O argumento agencia deve ser maior que Zero.");
+				throw new ArgumentException("O argumento agencia deve ser maior que Zero.","agencia");
 			    }
 
 		    	if(numero <= 0)
 			    {
 				//Lançamento de exceção de argumento
-				throw new ArgumentException("O argumento numero deve ser maior que Zero.");
+				throw new ArgumentException("O argumento numero deve ser maior que Zero.","agencia");
 			    }
 			//Agencia e Numero somente de leitura
 			Agencia = agencia; //Valor atribuido apenas no momento da criaçã odo objeto
