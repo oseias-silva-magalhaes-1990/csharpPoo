6e4168ea8ac4814d69b4ce4aba6280717508377d
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-01 16:40:48 -0300

    Create LeitorDeArquivo.cs

---
+++ ByteBank/08 - Herança e Interface/LeitorDeArquivo.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace ByteBank
+{
+    public class LeitorDeArquivo
+    {
+        public string Arquivo { get; }
+
+        public LeitorDeArquivo(string arquivo)
+        {
+                Arquivo = arquivo;
+                Console.WriteLine("Abrindo arquivo: " + arquivo);
+        }
+
+        public string LerProximaLinha()
+        {
+            Console.WriteLine("Lendo linha...");
+
+            throw new IOException();
+
+            return "linha do arquivo";
+        }
+
+        public void Fechar()
+        {
+                Console.WriteLine("Fechando arquivo.");
+        }
+    }
+}
