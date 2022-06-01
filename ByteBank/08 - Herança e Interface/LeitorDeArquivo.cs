c711190c51235b4edf4742c8ba03fc114ae2638e
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-01 17:09:03 -0300

    Update LeitorDeArquivo.cs

--- ByteBank/08 - Herança e Interface/LeitorDeArquivo.cs
+++ ByteBank/08 - Herança e Interface/LeitorDeArquivo.cs
@@ -8,8 +8,12 @@ namespace ByteBank
 
         public LeitorDeArquivo(string arquivo)
         {
-                Arquivo = arquivo;
-                Console.WriteLine("Abrindo arquivo: " + arquivo);
+            Arquivo = arquivo;
+            int divisor = 0;
+            int resultado = 10/divisor;
+
+            throw new FileNotFoundException();            
+            Console.WriteLine("Abrindo arquivo: " + arquivo);
         }
 
         public string LerProximaLinha()
@@ -23,12 +27,12 @@ namespace ByteBank
 
         public void Fechar()
         {
-                Console.WriteLine("Fechando arquivo.");
+            Console.WriteLine("Fechando arquivo.");
         }
         
         public void Dispose()
         {
-        
+            Console.WriteLine("Fechando arquivo.");
         }
     }
 }
