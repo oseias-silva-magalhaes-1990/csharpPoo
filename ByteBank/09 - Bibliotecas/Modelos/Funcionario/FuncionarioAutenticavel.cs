edbb1f58d203176e9aaf458d1ae356bf04bd738a
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:11:32 -0300

    Create FuncionarioAutenticavel.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/FuncionarioAutenticavel.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Bytebank.Modelos.Funcionarios
+{
+  public abstract class FuncionarioAutenticavel : Funcionario, Autenticavel
+  {
+    public string Senha {set; get;}
+  
+    public FuncionarioAutenticavel(double salario, string cpf): base (salario, cpf)
+    {
+      
+    }
+    
+    public bool Autenticar(string senha)
+    {
+      return Senha == senha;
+    }
+    
+  }
+}
