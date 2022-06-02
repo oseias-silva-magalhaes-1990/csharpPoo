513457c9d15b8c9f43669828d6a6810748b4f0e9
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:08:04 -0300

    Create Auxiliar.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/Auxiliar.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ByteBank.Funcionario
+{
+  public class Auxiliar : Funcionario //Herança - herda atributos da classe base
+  {
+    public Auxiliar(string cpf) : base (2000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
+    {
+      Console.WriteLine("Criando um Auxiliar");
+    }
+  
+    public override double GetBonificacao()//Definindo como override informa-se que o método é parte de uma classe pai
+    {
+      return Salario * 0.2;
+    }
+    
+    public override void AumentarSalario()
+    {
+      Salario *= 1.1;
+    }
+  }
+}
