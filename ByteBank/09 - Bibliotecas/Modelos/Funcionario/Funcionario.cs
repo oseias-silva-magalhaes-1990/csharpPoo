269eaf4673c9ca48b97ff264b426eb4715771ab0
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:11:08 -0300

    Create Funcionario.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/Funcionario.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace ByteBank.Funcionario
+{
+  public abstract class Funcionario//Classe abstrata pois não deve ser instanciada
+  {
+    public static int TotalDeFuncionarios{get; private set;}
+    
+    public string Nome {get; set;}//public - visível em todo lugar;
+    public string CPF {get; private set;}//private - apenas visível dentro da classe;
+    public double Salario {get; protected set;}//protected - visível dentro da classe e também para as filhas;
+    public string Senha {get; private set;}
+    
+    public Funcionario(double salario, string cpf)
+    {
+      CPF = cpf;
+      Salario = salario;
+      
+      TotalDeFuncionarios++;
+      Console.WriteLine("Criando um Funcionário");
+    }
+    
+    public abstract double GetBonificacao();
+    
+    public abstract void AumentarSalario();
+      
+    /*
+    public virtual double GetBonificacao()//Definindo como virtual o método pode ser utilziado por uma classe derivada e sofrer mudanças em sua estrutura
+    {
+      return Salario * 0.10;
+    }
+    
+    public virtual void AumentarSalario()
+    {
+      Salario *= 1.1;
+    }
+    */
+    
+  }
+}
