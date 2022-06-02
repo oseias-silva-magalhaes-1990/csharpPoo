e6d437bb9b1a1e591495328d9dcba084a84b147f
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:10:14 -0300

    Create Designer.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/Designer.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace ByteBank.Modelos.Funcionario
+{
+  public class Designer : Funcionario //Herança - herda atributos da classe base
+  {
+    public Designer(string cpf) : base (3000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
+    {
+      Console.WriteLine("Criando um Designer");
+    }
+  
+    public override double GetBonificacao()//Definindo como override informa-se que o método é parte de uma classe pai
+    {
+      return Salario * 0.17;
+    }
+    
+    public override void AumentarSalario()
+    {
+      Salario *= 1.11;
+    }
+  }
+}
