74f804cd4f7f8dec790edfb4b0801e521914c96e
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:19:07 -0300

    Create SistemaAgencia.cs

---
+++ ByteBank/09 - Bibliotecas/SistemaAgencia.cs
@@ -0,0 +1,15 @@
+using System;
+using ByteBank.Modelos;
+using ByteBank.Sistema;
+
+namespace ByteBank.SistemaAgencia
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      //Início de Programa
+      Console.ReadLine();
+    }
+  }
+}
