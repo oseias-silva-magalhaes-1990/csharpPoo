2744368baad29a8b40c2c1e89e8e09c21ae30ea3
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:06:08 -0300

    Update SaldoInsuficienteException.cs

--- ByteBank/08 - Herança e Interface/SaldoInsuficienteException.cs
+++ ByteBank/08 - Herança e Interface/SaldoInsuficienteException.cs
@@ -1,35 +1,31 @@
-
-
 using System;
 
 namespace ByteBank
 {
   class SaldoInsuficienteException : OperacaoFinanceiraException
   {
-    
     public double Saldo{ get;}
     public double ValorSaque{get;}
-    
+
     public SaldoInsuficienteException()
     {
-    
     }
-    
+
     public SaldoInsuficienteException(double saldo, double valorSaque)
-      : this ("Tentativa de saque no valor de " + valorSaque + " em uma conta com saldo de " + saldo)
+    : this ("Tentativa de saque no valor de " + valorSaque + " em uma conta com saldo de " + saldo)
     {
       Saldo = saldo;
       ValorSaque = valorSaque;
     }
-    
+
     public SaldoInsuficienteException(string mensagem)
-            : base(mensagem)
-        {
-        }
-        
+    : base(mensagem)
+    {
+    }
+
     public SaldoInsuficienteException(string mensagem, Exception excecaoInterna)
-            : base(mensagem, excecaoInterna)
-        {
-        }
+    : base(mensagem, excecaoInterna)
+    {
+    }
   }
 }
