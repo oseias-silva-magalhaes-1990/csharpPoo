387bc35d3739531a874fcdf32f4bdebf3f0c95e0
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:09:18 -0300

    Create Desenvolvedor.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/Desenvolvedor.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace ByteBank.Funcionario
+{
+  public class Desenvolvedor : Funcionario //Herança - herda atributos da classe base
+  {
+    
+    public Desenvolvedor(string cpf) : base (5000, cpf)//Atribuição de parâmetro para obedecer regra do construtor da classe base
+    {
+      Console.WriteLine("Criando um Desenvolvedor");
+    }
+  
+    public override double GetBonificacao()//Definindo como override informa-se que o método é parte de uma classe pai
+    {
+      return Salario * 0.1;
+    }
+    
+    public override void AumentarSalario()
+    {
+      Salario *= 0.15;
+    }
+  }
+}
