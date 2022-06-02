6f5e6564fb6fbc72f12eb9f4d689ff20fec0c395
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:04:27 -0300

    Create ParceiroComercial.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/ParceiroComercial.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ByteBank.Modelos
+{
+  public class ParceiroComercial : IAutenticavel
+  {
+    public string Senha {get; set;}
+    public bool Autenticar(string senha)
+    {
+      return Senha == senha;
+    }
+  }
+}
