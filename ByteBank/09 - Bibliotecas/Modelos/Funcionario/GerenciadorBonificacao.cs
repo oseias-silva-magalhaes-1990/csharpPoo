31f7190953d30f8e3271ceea9f4bbfffd5f00f27
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-02 15:12:24 -0300

    Create GerenciadorBonificacao.cs

---
+++ ByteBank/09 - Bibliotecas/Modelos/Funcionario/GerenciadorBonificacao.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ByteBank.Modelos.Funcionario
+{
+  public class GerenciadorBonificacao
+  {
+    private double _totalBonificacao; 
+  
+    public void Registrar(Funcionario func){
+      _totalBonificacao += func.GetBonificacao();
+    }
+    
+    public double GetTotalBonificacao(){
+      return _totalBonificacao;
+    }
+  }
+}
