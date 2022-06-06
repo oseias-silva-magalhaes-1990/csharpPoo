9deeb2ab868b9e6f68ffae5ddfec177031b1575e
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-06 14:56:43 -0300

    Update SistemaAgencia.cs

--- ByteBank/09 - Bibliotecas/SistemaAgencia/SistemaAgencia.cs
+++ ByteBank/09 - Bibliotecas/SistemaAgencia/SistemaAgencia.cs
@@ -1,5 +1,6 @@
 using System;
 using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
 using ByteBank.Sistema;
 
 namespace ByteBank.SistemaAgencia
@@ -8,7 +9,14 @@ namespace ByteBank.SistemaAgencia
   {
     static void Main(string[] args)
     {
-      //Início de Programa
+      //Em Referências deve-se adicionar a referência ByteBank.Modelos dentro de Projetos
+      ContaCorrente conta = new ContaCorrente(847, 489754);
+      
+      AutenticacaoHelper
+      
+      Funcionario funcionario = null;
+      Console.WriteLine(conta.Numero);
+      
       Console.ReadLine();
     }
   }
