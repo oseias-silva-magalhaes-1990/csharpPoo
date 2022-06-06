056cf154b34e2a05675bdb313f66c76221ac3e79
Oseias Silva Chagas Magalhaes <37406978+oseias-silva-magalhaes-1990@users.noreply.github.com>
2022-06-06 15:14:22 -0300

    Update SistemaAgencia.cs

--- ByteBank/09 - Bibliotecas/SistemaAgencia/SistemaAgencia.cs
+++ ByteBank/09 - Bibliotecas/SistemaAgencia/SistemaAgencia.cs
@@ -12,8 +12,6 @@ namespace ByteBank.SistemaAgencia
       //Em Referências no Painel Lateral deve-se adicionar a referência ByteBank.Modelos dentro de Projetos
       ContaCorrente conta = new ContaCorrente(847, 489754);
       
-      AutenticacaoHelper
-      
       Funcionario funcionario = null;
       Console.WriteLine(conta.Numero);
       
